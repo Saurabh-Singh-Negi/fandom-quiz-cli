511bf56c38e377bb87b00a3bd619088696f64461
saurabh negi <saurabhnegi0007@gmail.com>
2021-02-13 13:59:41 +0530

    feat: check if you made it to high scorers list

--- index.js
+++ index.js
@@ -55,4 +55,27 @@ for(var i=0;i<questionArr.length;i++) {
     check(questionArr[i].question,questionArr[i].answer); 
 }
 
+var highScores = [
+    {
+      name: "ABC",
+      score: 5
+    },
+    {
+      name: "XYZ",
+      score: 4
+    }
+  ];
+  
+  console.log("High scorers");
+  for(let i=0;i<highScores.length;i++) {
+    console.log(highScores[i].name + " " + highScores[i].score )
+  }
+  
+  for(let i=0;i<highScores.length;i++) {
+    if(score>highScores[i].score) {
+      console.log("You've made it to the high scorers list");
+    }
+    break;
+  }
+
 console.log("Your final score is",score);
