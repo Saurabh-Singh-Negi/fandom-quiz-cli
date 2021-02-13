1a8810ccde1aa962bd0b049a0110e71978dee20c
saurabh negi <saurabhnegi0007@gmail.com>
2021-02-13 13:42:08 +0530

    initial commit
    Added questions for the quiz

---
+++ index.js
@@ -0,0 +1,31 @@
+var questionArr = [
+    {
+      question: "Captain of Indian Football team?(first name) ",
+      answer: "sunil"
+    },
+  
+    {
+      question: "Which country will host the FIFA world cup 2022? ",
+      answer: "qatar"
+    },
+  
+    {
+      question: "Current FIFA champions? ",
+      answer: "france"
+    },
+  
+    {
+      question: "Ballon d'Or 2020 winner?(only last name) ",
+      answer: "ronaldo"
+    },
+  
+    {
+      question: "Which club has won the most champions league title? ",
+      answer: "real madrid"
+    },
+  
+    {
+      question: "Which country has won the most world cups? ",
+      answer: "brazil"
+    }
+  ];
