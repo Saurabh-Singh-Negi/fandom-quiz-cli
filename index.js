716f186a0d70cd70e7c2077c233e94994c287465
saurabh negi <saurabhnegi0007@gmail.com>
2021-02-15 13:38:33 +0530

    style: added CHALK library

--- index.js
+++ index.js
@@ -1,95 +1,100 @@
 var readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
-var userName = readlineSync.question("Enter your name ");
-console.log("Welcome to the football quiz",userName);
+const blue = chalk.blue.bold;
+const red = chalk.red.bold;
+
+var userName = readlineSync.question(red("Enter your name "));
+console.log(blue("Welcome to the football quiz",userName));
 
 var questionArr = [
-    {
-      question: "Captain of Indian Football team?(first name) ",
-      answer: "sunil"
-    },
-  
-    {
-      question: "Which country will host the FIFA world cup 2022? ",
-      answer: "qatar"
-    },
-  
-    {
-      question: "Current FIFA champions? ",
-      answer: "france"
-    },
-  
-    {
-      question: "Ballon d'Or 2020 winner?(only last name) ",
-      answer: "ronaldo"
-    },
-  
-    {
-      question: "Which club has won the most champions league title? ",
-      answer: "real madrid"
-    },
-  
-    {
-      question: "Which country has won the most world cups? ",
-      answer: "brazil"
-    }
-  ];
+  {
+    question: "Captain of Indian Football team?(first name) ",
+    answer: "sunil"
+  },
+
+  {
+    question: "Which country will host the FIFA world cup 2022? ",
+    answer: "qatar"
+  },
+
+  {
+    question: "Current FIFA champions? ",
+    answer: "france"
+  },
+
+  {
+    question: "Ballon d'Or 2020 winner?(only last name) ",
+    answer: "ronaldo"
+  },
+
+  {
+    question: "Which club has won the most champions league title? ",
+    answer: "real madrid"
+  },
+
+  {
+    question: "Which country has won the most world cups? ",
+    answer: "brazil"
+  }
+];
 
 var score = 0;
 function check(question, answer) {
-  var userAns = readlineSync.question(question);
+  var userAns = readlineSync.question(red(question));
   if(userAns === answer) {
-    console.log("Yay! you are right");
+    console.log(blue("Yay! you are right"));
     score++;
   }
 
   else {
-    console.log("you got it wrong");
+    console.log(blue("you got it wrong"));
   }
 
-  console.log("Current Score:", score);
-  console.log("-----------------------");
+  console.log(blue("Current Score:", score));
+  console.log(blue("-----------------------"));
 }
 
 for(var i=0;i<questionArr.length;i++) {
-    if(i==3 && score==3) {
-      console.log("<<<< Welcome to level 2 >>>>>");
-    }
-    if(i<3) {
-      check(questionArr[i].question,questionArr[i].answer);
-    }
-  
-    else if(i==3 && score<3) {
-      console.log("You couldn't make it to level 2");
-      break;
-    }
-  
-    else {
-      check(questionArr[i].question,questionArr[i].answer);
-    } 
+  if(i==3 && score==3) {
+    console.log(blue("<<<< Welcome to level 2 >>>>>"));
+  }
+  if(i<3) {
+    check(questionArr[i].question,questionArr[i].answer);
   }
 
-var highScores = [
-    {
-      name: "ABC",
-      score: 5
-    },
-    {
-      name: "XYZ",
-      score: 4
-    }
-  ];
-  
-  console.log("High scorers");
-  for(let i=0;i<highScores.length;i++) {
-    console.log(highScores[i].name + " " + highScores[i].score )
-  }
-  
-  for(let i=0;i<highScores.length;i++) {
-    if(score>highScores[i].score) {
-      console.log("You've made it to the high scorers list");
-    }
+  else if(i==3 && score<3) {
+    console.log(blue("You couldn't make it to level 2"));
     break;
   }
 
-console.log("Your final score is",score);
+  else {
+    check(questionArr[i].question,questionArr[i].answer);
+  } 
+}
+
+var highScores = [
+  {
+    name: "ABC",
+    score: 5
+  },
+  {
+    name: "XYZ",
+    score: 4
+  }
+];
+
+console.log("High scorers");
+for(let i=0;i<highScores.length;i++) {
+  console.log(highScores[i].name + " " + highScores[i].score )
+}
+
+for(let i=0;i<highScores.length;i++) {
+  if(score>highScores[i].score) {
+    console.log("You've made it to the high scorers list");
+  }
+  break;
+}
+
+console.log(blue("Your final score is",score));
+
